f94efe82006b0b859230ac2ee8382d6b156a81c2
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-23 00:31:58 +0600

    added all fils code lines

---
+++ task-practic.js
@@ -0,0 +1,109 @@
+// // let str = "hello";
+// // let arr = ['h', 'e', '1', '1', '0'];
+
+// // // console.log(str.length);
+// // // console.log(arr.length);
+
+// // // console.log(str[0]);
+// // // console.log(arr[0]);
+  
+
+// // // this is wrong 
+// // // str.push('!');
+
+// // // this is right 
+// // str += '!';
+// // console.log(str)
+// // arr.push('5');
+// // console.log(arr)
+
+
+
+// // let str = "JavaScript";
+
+// // let arr = str.split('');
+// // // console.log(arr);
+
+// // let theArr = ["c", "a", "t"];
+
+// // let singleStr = theArr.join('');
+// // console.log(singleStr)
+
+
+// // let name = "  JavaScript";
+
+
+// // let upperVersion = name.toUpperCase();
+// // // console.log(upperVersion)
+
+// // let lowerVersion = name.toLowerCase();
+// // // console.log(lowerVersion)
+
+// // // name.trim();
+// // // console.log(name);
+
+// // let trimmedName = name.trim();
+// // console.log(trimmedName)
+
+
+// // let sentence = "Learning JavaScript is fun!";
+
+// // let usingSlice = sentence.slice(0, 9);
+// // // console.log(usingSlice)
+
+// // let negativeSlice = sentence.slice(-4);
+// // console.log(negativeSlice)
+
+
+// // let a = "Hello";
+// // let b = "World";
+
+// // let useConcat = a.concat(" ", b);
+// // console.log(useConcat)
+
+
+// // let add = a + " " + b;
+// // console.log(add)
+
+// // let add2 = `${a} ${b}`;
+// // console.log(add2)
+
+
+
+
+// let stringUse = "JavaScript";
+
+// // // let reverseString = str.reverse();
+// // // console.log(reverseString)
+
+// // let useSplit = str.split(" ");
+// // // console.log(useSplit)
+
+// // let useJoin = str.join(str);
+// // console.log(useJoin)
+
+// let splitUse = stringUse.split('');
+// console.log(splitUse)
+
+
+// const reversedUse = splitUse.reverse();
+// console.log(reversedUse)
+
+// const joined = reversedUse.join('');
+// console.log(joined)
+
+
+
+
+// let student = {
+//     name:"Alex",
+//     age: 34,
+//     grade: "GPA: 4.5",
+//     isEnrolled: true,
+// };
+
+// // console.log(student);
+
+// console.log(student.name);
+
+
